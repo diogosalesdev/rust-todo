8abfa2ee05b0cf1e6263f00c8d040cf3b83b5c92
ehgkj <diogo.sales.ext@bayer.com>
2023-07-30 12:59:17 -0300

    feat: Edit by id function created
    Update CRUD method creatd with function called edit by id

--- src/handler.rs
+++ src/handler.rs
@@ -1,3 +1,5 @@
+use std::path;
+
 use crate::{
     model::{AppState, QueryOptions, Todo, UpdateTodoSchema},
     response::{GenericResponse, SingleTodoResponse, TodoData, TodoListResponse},
@@ -99,5 +101,58 @@ pub async fn get_todo_handler(
         status: "success".to_string(),
         data: TodoData { todo: todo.clone() },
     };
+    HttpResponse::Ok().json(json_response)
+}
+
+#[patch("/todos/{id}")]
+pub async fn edit_todo_handler(
+    path: web::Path<String>,
+    body: web::Json<UpdateTodoSchema>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let mut vec = data.todo_db.lock().unwrap();
+
+    let id = path.into_inner();
+    let todo = vec.iter_mut().find(|todo| todo.id == Some(id.to_owned()));
+
+    if todo.is_none() {
+        let error_response = GenericResponse {
+            status: "fail".to_string(),
+            message: format!("Todo with ID: {} not found", id),
+        };
+        return HttpResponse::NotFound().json(error_response)
+    }
+
+    let todo = todo.unwrap();
+    let datetime = Utc::now();
+    let title = body.title.to_owned().unwrap_or(todo.title.to_owned());
+    let content = body.content.to_owned().unwrap_or(todo.content.to_owned());
+    let payload = Todo {
+        id: todo.id.to_owned(),
+        title: if !title.is_empty() {
+            title
+        } else {
+            todo.title.to_owned()
+        },
+        content: if !content.is_empty() {
+            content
+        } else {
+            todo.content.to_owned()
+        },
+        completed: if body.completed.is_some() {
+            body.completed
+        } else {
+            todo.completed
+        },
+        createdAt: todo.createdAt,
+        updatedAt: Some(datetime),
+    };
+    *todo = payload;
+
+    let json_response = SingleTodoResponse {
+        status: "success".to_string(),
+        data: TodoData { todo: todo.clone() },
+    };
+
     HttpResponse::Ok().json(json_response)
 }
